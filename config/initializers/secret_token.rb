ebdd86d97501ff881a4b3945cce473b28044bb6a
youhouz <ramsi.ferkous@live.fr>
2014-03-24 14:47:01 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testfonct::Application.config.secret_key_base = '6138b65121bb2cd4f2c2518960c4a5bdbccdcb935a810528d795126281371fbaf147245604113c50204167791cc1d64a21021cd02f582617b436ddf5acabef58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testfonct::Application.config.session_store :cookie_store, key: '_testfonct_session'
